4d8ccac168938733a375cb1e976922f5a56b7227
SergioRAIS <130254963+SergioRAIS@users.noreply.github.com>
2023-06-20 21:53:08 +0300

    дз2

--- Program.cs
+++ Program.cs
@@ -1,19 +1,17 @@
-﻿Console.WriteLine("Введите число: ");
-string numbar = Console.ReadLine() ?? "";
-int len = numbar.Length;
-
-if (len == 5)
+﻿
+int x1 = ReadInt("Введите координату 1 модуля A: ");
+int y1 = ReadInt("Введите координату 2 модуля A: ");
+int z1 = ReadInt("Введите координату 3 модуля A: ");
+int x2 = ReadInt("Введите координату 1 модуля B: ");
+int y2 = ReadInt("Введите координату 2 модуля B: ");
+int z2 = ReadInt("Введите координату 3 модуля B: ");
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z1 - z2;
+double length = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine($"расстояние отрезка {length}");
+int ReadInt(string message)
 {
-    if (numbar[0] == numbar[4] && numbar[1] == numbar[3])
-    {
-        Console.WriteLine($"{numbar} - ДА!");
-    }
-    else
-    {
-        Console.WriteLine($"{numbar} - нет!");
-    }
-}
-else
-{
-    Console.WriteLine($"ОШИБКА: {numbar} - не пятизначное");
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
